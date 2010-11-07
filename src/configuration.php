576b7b0f1bde827c72c18cf5664625499f63e970
Cam <cam@someemailthatdoesntwork.com>
2010-11-07 14:36:56 -0800

    added an inital page layout (using stand-in images)

--- src/configuration.php
+++ src/configuration.php
@@ -3,7 +3,7 @@ class JConfig {
 /* Site Settings */
 var $offline = '0';
 var $offline_message = 'This site is down for maintenance.<br /> Please check back again soon.';
-var $sitename = 'Bokka\'s Mobile Hair Dressing';
+var $sitename = '';
 var $editor = 'tinymce';
 var $list_limit = '20';
 var $legacy = '0';
--- src/templates/bokkas-website/index.php
+++ src/templates/bokkas-website/index.php
@@ -3,8 +3,17 @@
 <head>
 <jdoc:include type="head" />
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/bokkas-website/css/reset-min.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/bokkas-website/css/rebecca-cheri.css" type="text/css" />
 </head>
 <body>
-	bokkas website!
+	<div id="business-logo">
+		Rebecca Cheri
+	</div>
+	<div id="toolbar">
+		<jdoc:include type="modules" name="toolbar" style="xhtml" headerLevel="3" />
+	</div>
+	<div id="main">
+		<jdoc:include type="component" />
+	</div>
 </body>
 </html>
