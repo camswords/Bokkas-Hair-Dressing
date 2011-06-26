72486a9999cb9cf9ea35ebbc375fd1ab3e21487f
Cam <cam@someemailthatdoesntwork.com>
2011-06-26 01:04:22 +0100

    renamed bokkas website to Rebecca Cheri, fixed issue where server wasnt being stopped on deployment, ensured that log files are not deployed.

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -12,22 +12,21 @@ rm RebeccaCheri.zip --force
 
 # create a new deployment archive
 cd ../..
-zip -r ./Bokkas-Hair-Dressing/deployment/RebeccaCheri.zip Bokkas-Hair-Dressing -x "**/.git/*" -x "**/tmp/*" -x "**/deployment/*" -x "**/eclipse/*" -x "**/vmsetup/*" -x "**/.settings/*" -x "**/test/*" -x "**/.classpath" -x "**/.project" -x "**/.gitignore"
-cd ./Bokkas-Hair-Dressing/deployment
+zip -r ./RebeccaCheri/deployment/RebeccaCheri.zip RebeccaCheri -x "**/logs/*" -x "**/.git/*" -x "**/tmp/*" -x "**/deployment/*" -x "**/eclipse/*" -x "**/vmsetup/*" -x "**/.settings/*" -x "**/test/*" -x "**/.classpath" -x "**/.project" -x "**/.gitignore"
+cd ./RebeccaCheri/deployment
 
 # copy latest artifact to the remote server, unzip
 scp RebeccaCheri.zip bokka@$SERVER_NAME:~
 
 # stop the play server
-ssh bokka@$SERVER_NAME '/var/lib/play-1.2.1/play stop RebeccaCheri'
+ssh bokka@$SERVER_NAME 'sudo /var/lib/play-1.2.1/play stop RebeccaCheri'
 
 # remove the old rebecca cheri
-ssh bokka@$SERVER_NAME 'rm RebeccaCheri_previous -rf'
+ssh bokka@$SERVER_NAME 'sudo rm RebeccaCheri_previous -rf'
 ssh bokka@$SERVER_NAME 'mv RebeccaCheri RebeccaCheri_previous'
 
 # extract the new rebecca cheri
 ssh bokka@$SERVER_NAME 'unzip RebeccaCheri.zip'
-ssh bokka@$SERVER_NAME 'mv Bokkas-Hair-Dressing RebeccaCheri'
 
 # start the play server
-ssh bokka@$SERVER_NAME '/var/lib/play-1.2.1/play start RebeccaCheri'
+ssh bokka@$SERVER_NAME 'sudo /var/lib/play-1.2.1/play start RebeccaCheri --%prod'
