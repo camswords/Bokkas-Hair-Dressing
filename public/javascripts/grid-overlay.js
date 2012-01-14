43f51966baf4e0a50ceb52822c7c989402a29c96
Cam <cam@someemailthatdoesntwork.com>
2012-01-14 17:20:47 +0000

    added an overlay for the 960 css that i am in the middle of converting to.

---
+++ public/javascripts/grid-overlay.js
@@ -0,0 +1,27 @@
+
+var Overlay = {};
+
+Overlay.display = function() {
+	
+	var html = '<div class="container_12" style="position:absolute; z-index: 5000; left: 50%; margin-left: -480px; top: 0; height: 650px;">' + 
+	'<div class="grid_1" style="border: dashed red 1px; min-height: 100%; font-size: 70%;">1</div>' +
+	'<div class="grid_1" style="border: dashed red 1px; min-height: 100%; font-size: 70%;">2</div>' +
+	'<div class="grid_1" style="border: dashed red 1px; min-height: 100%; font-size: 70%;">3</div>' +
+	'<div class="grid_1" style="border: dashed red 1px; min-height: 100%; font-size: 70%;">4</div>' +
+	'<div class="grid_1" style="border: dashed red 1px; min-height: 100%; font-size: 70%;">5</div>' +
+	'<div class="grid_1" style="border: dashed red 1px; min-height: 100%; font-size: 70%;">6</div>' +
+	'<div class="grid_1" style="border: dashed red 1px; min-height: 100%; font-size: 70%;">7</div>' +
+	'<div class="grid_1" style="border: dashed red 1px; min-height: 100%; font-size: 70%;">8</div>' +
+	'<div class="grid_1" style="border: dashed red 1px; min-height: 100%; font-size: 70%;">9</div>' +
+	'<div class="grid_1" style="border: dashed red 1px; min-height: 100%; font-size: 70%;">10</div>' +
+	'<div class="grid_1" style="border: dashed red 1px; min-height: 100%; font-size: 70%;">11</div>' +
+	'</div>';
+	$(document.body).append(html);
+};
+
+
+$(document).ready(function() {
+	Overlay.display();
+});
+
+
